7c5e7e14a1947b0aeb23bca63b683175520a12b5
MaximTurchin <maxas1488@gmail.com>
2026-02-26 12:11:27 +0300

    adding docker (work container)

--- backend/src/routes/server.ts
+++ backend/src/routes/server.ts
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 app.get('/', (req, res) => {
